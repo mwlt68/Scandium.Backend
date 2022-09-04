a7c7219a75b76f1714581703f74b71b0745ca2e2
mwlt68 <mwltgr@gmail.com>
2022-09-04 13:22:29 +0300

    User entity password update. Md5Hash add

---
+++ Migrations/20220904101456_UserPasswordUpdate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Scandium.Migrations
+{
+    public partial class UserPasswordUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(25)",
+                oldMaxLength: 25);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "character varying(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
