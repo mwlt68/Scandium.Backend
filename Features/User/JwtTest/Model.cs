329281f1204182fc7eb5ac6965629b30cdeaee8d
mwlt68 <mwltgr@gmail.com>
2023-03-22 23:19:14 +0300

    Jwt add

---
+++ Features/User/JwtTest/Model.cs
@@ -0,0 +1,13 @@
+
+namespace Scandium.Features.User.JwtTest
+{
+    public class Request
+    {
+        public string? Username { get; set; }   
+    }
+
+    public class Response
+    {
+        public int Id { get; set; }
+    }
+}
