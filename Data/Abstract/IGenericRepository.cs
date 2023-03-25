ae6ae63740aa6cd9d7f4f1bb2ef502bebf977f58
mwlt68 <mwltgr@gmail.com>
2023-03-25 18:31:36 +0300

    Generic repository update

--- Data/Abstract/IGenericRepository.cs
+++ Data/Abstract/IGenericRepository.cs
@@ -1,14 +1,17 @@
 using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 using Scandium.Model;
 
 namespace Scandium.Data.Abstract
 {
     public interface IGenericRepository<TEntity> where TEntity : BaseEntity, new()
     {
+        DbSet<TEntity> GetDbSet{get;}
         Task<List<TEntity>> GetListAsync(Expression<Func<TEntity, bool>>? filter = null);
 
         Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> filter);
         Task<TEntity?> GetByIdAsync(Guid id);
+        Task<TEntity> GetByIdThrowAsync(Guid id);
 
         Task<TEntity> AddAsync(TEntity entity);
         Task<bool> AnyAsync(Expression<Func<TEntity, bool>> filter);
--- Model/Entities/BaseEntity.cs
+++ Model/Entities/BaseEntity.cs
@@ -3,6 +3,6 @@ namespace Scandium.Model
     public abstract class BaseEntity
     {
         public Guid Id { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } =DateTime.UtcNow;
     }
 }
