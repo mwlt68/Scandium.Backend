e0edce93f5d573d720e195db5582b503ab1c640e
mwlt68 <mwltgr@gmail.com>
2023-10-22 14:36:42 +0300

    Content convert to byte array in Message entity

---
+++ Migrations/20231022095902_UpdateMessageContent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Scandium.Migrations
+{
+    public partial class UpdateMessageContent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Messages");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Contents",
+                table: "Messages",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Contents",
+                table: "Messages");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Messages",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
